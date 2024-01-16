23fa37adff8227fe908e994241e850ad3906c9bd
Egidijus <151669057+EgidijusV1991@users.noreply.github.com>
2024-01-16 21:31:26 +0200

    Add playGame function, compares selections anddeclares outcome

--- javascript.js
+++ javascript.js
@@ -16,21 +16,56 @@ function getComputerChoice(){
     }
     return computerChoice
 }
-console.log(getComputerChoice())
 
+let computerChoice = getComputerChoice();
+console.log("computer "+computerChoice)
 let playerSelection = prompt("Choose: Rock-Paper-Scissors").toLowerCase()
-console.log(playerSelection)
 checkInput()
+
+
 function checkInput(){
     if(playerSelection === ""){
         playerSelection = prompt("Choose: Rock-Paper-Scissors2").toLowerCase()
         checkInput()
     }
     else if(playerSelection == "rock" || playerSelection == "scissors" || playerSelection == "papper"){
-        console.log(playerSelection)
+        playGame()
     }
     else if (playerSelection != null){
         playerSelection = prompt("Choose: Rock-Paper-Scissors2").toLowerCase()
         checkInput()
+    }
+}
+console.log("player "+playerSelection)
+
+function playGame(){
+    if(playerSelection == computerChoice){
+        console.log("It's a draw!")
+    }
+    switch(true){
+        case playerSelection == "rock" && computerChoice == "papper":{
+            console.log("You lost!1 :(")
+            break
+        }
+        case playerSelection == "rock" && computerChoice == "scissors":{
+            console.log("You won!1 :)")
+            break
+        }
+        case playerSelection == "papper" && computerChoice == "scissors":{
+            console.log("You lost!2 :(")
+            break
+        }
+        case playerSelection == "papper" && computerChoice == "rock":{
+            console.log("You won!2 :)")
+            break
+        }
+        case playerSelection == "scissors" && computerChoice == "papper":{
+            console.log("You won!3 :)")
+            break
+        }
+        case playerSelection == "scissors" && computerChoice == "rock":{
+            console.log("You lost!3 :(")
+            break
+        }
     }
 }
